4129d5aef4ba1718bfd031f2cd6184530cfe66fc
udw4817 <udw4817@gmail.com>
2016-10-11 12:40:45 +0100

    updated files

--- myflask.py
+++ myflask.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+albums = [{
+    'artist': 'Madonna',
+    'title': 'True Blue',
+    'year': '1987'
+},
+
+    {
+        'artist': 'Britney Spears',
+        'title': 'Hit me baby one more time',
+        'year': '1998'
+    },
+
+    {
+        'artist': 'Pink Floyd',
+        'title': 'The Wall',
+        'year': 'Around 1976'
+    }]
+
+genres = ['rock', 'blues', 'pop']
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Hello, World!'
+
+
+@app.route('/albums')
+def list_albums():
+    return render_template('index.html', albums=albums, genres=genres)
 
 
 if __name__ == '__main__':
